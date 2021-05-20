ba1bfbddf4e416928b385d9ea463f78dc63a3bb5
Dmitry Kalalb <gdydhd0134h@gmail.com>
2021-05-20 21:41:36 +0200

    select temp

---
+++ src/components/competition/team/Teams.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="component">
+    <div class="teams">
+      <p class="teams__head">Teams Menegment</p>
+      <div class="teams__content">
+        <i class="teams__text">Select a skill</i>
+        <CustomSelect
+          :options="['go', 'python', 'rust', 'javascript']"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CustomSelect from '@/components/competition/team/CustomSelect'
+
+export default {
+  name: "Teams",
+  components: {
+    CustomSelect
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
